7d4d617fa9a629a3cac32b18905e77b6d9d72727
dolly101599 <subhashnimmala01@gmail.com>
2024-05-04 15:57:45 +0530

    items per page change

--- aDme_Assesment/src/Components/Gallery.jsx
+++ aDme_Assesment/src/Components/Gallery.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 
-const itemsPerPage = 10;
+const itemsPerPage = 12;
 
 const GalleryItem = ({ item }) => (
   <div className="rounded-lg overflow-hidden shadow-lg">
@@ -106,7 +106,7 @@ const Gallery = () => {
         <h1 className="text-3xl font-semibold text-gray-800 inline-block ml-2">Gallery App</h1>
       </div>
       <div className="mb-8">
-        <img src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fHww" alt="Header" className="w-full rounded-lg shadow-lg" />
+        <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhlYWRlciUyMHBob3RvJTIwaGR8ZW58MHx8MHx8fDA%3D" alt="Header" className="w-full rounded-lg shadow-lg" />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {currentItems.map((item) => (
